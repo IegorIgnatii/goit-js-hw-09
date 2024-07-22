abffd980ff91c4789a5914399f2e792670975e9c
IegorIgnatii <165487943+IegorIgnatii@users.noreply.github.com>
2024-07-22 13:50:37 +0300

    go

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,24 +1,16 @@
-const refs = {
-  formEl: document.querySelector('.feedback-form'),
-};
-
+const formEl = document.querySelector('.feedback-form');
 const LOCAL_KEY = 'feedback-form-state';
 
-const formData = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {
+const formData = {
   email: '',
   message: '',
 };
 
-refs.formEl.addEventListener('input', e => {
-  formData[e.target.name] = e.target.value.trim();
+formEl.addEventListener('input', () => {
+  formData.email = formEl.elements.email.value.trim();
+  formData.message = formEl.elements.message.value.trim();
 
-  saveToLs(LOCAL_KEY, formData);
-
-  function saveToLs(key, value) {
-    const json = JSON.stringify(value);
-    localStorage.setItem(key, json);
-    console.log(json);
-  }
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(formData));
 });
 
 formEl.addEventListener('submit', event => {
@@ -36,23 +28,9 @@ formEl.addEventListener('submit', event => {
   formEl.reset();
 });
 
-function loadFromLs(key) {
-  const json = localStorage.getItem(key);
-  try {
-    const data = JSON.parse(json);
-    return data;
-  } catch {
-    return json;
-  }
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const loadForms = JSON.parse(localStorage.getItem(LOCAL_KEY));
 
-function setDataToForm() {
-  const loadStorage = loadFromLs('feedback-form-state');
-  if (loadStorage === null) return;
-  const keys = Object.keys(formData);
-
-  keys.forEach(key => {
-    refs.formEl.elements[key].value = loadStorage[key];
-  });
-}
-setDataToForm();
+  formEl.elements.email.value = loadForms?.email ?? '';
+  formEl.elements.message.value = loadForms?.message ?? '';
+});
